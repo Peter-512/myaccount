6f03ded191f3a17af25ba17da059011c2d0e158b
Peter <peter.buschenreiter@gmail.com>
2020-10-11 21:30:33 +0200

    Updates

---
+++ precache-manifest.6bb2051a28a891160c324f03fadfe46d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca345b857a1422244488b7e574c575c1",
+    "url": "/randomquotemachine/index.html"
+  },
+  {
+    "revision": "ff0f0de473d3f19ad623",
+    "url": "/randomquotemachine/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86b4f1b1dc78bc8aa5a2",
+    "url": "/randomquotemachine/static/js/2.dddd10af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomquotemachine/static/js/2.dddd10af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0f0de473d3f19ad623",
+    "url": "/randomquotemachine/static/js/main.bc6f8627.chunk.js"
+  },
+  {
+    "revision": "7820c1bcb9bcce5e36e1",
+    "url": "/randomquotemachine/static/js/runtime-main.29ae4a45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomquotemachine/static/media/logo.5d5d9eef.svg"
+  }
+]);
